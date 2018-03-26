a251adcfbc43c0fcfbdd4f6d04985bf544caf7b9
James <james_gower@outlook.com>
2018-03-26 14:50:39 +0100

    updated survey template, updated package.json to run sendgrid webhook for updating survey query responses, and created and tested survey response queries

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -8,6 +11,45 @@ const Survey = mongoose.model('surveys');
 module.exports = app => {
 	app.get('/api/surveys/thanks', (req, res) => {
 		res.send('Thanks for voting!');
+	});
+
+	app.post('/api/surveys/webhooks', (req, res) => {
+		const p = new Path('/api/surveys/:surveyId/:choice');
+		_.chain(req.body)
+			.map(({ email, url }) => {
+				const match = p.test(new URL(url).pathname);
+				if (match) {
+					return { email, surveyId: match.surveyId, choice: match.choice };
+				}
+			})
+			.compact()
+			.uniqBy('email', 'surveyId')
+			.each(({ surveyId, email, choice }) => {
+				Survey.updateOne(
+					//Look at survey collection and find & update the collection
+					{
+						_id: surveyId, //mongoDB needs the id to be '_id'
+						recipients: {
+							$elemMatch: {
+								//find recipient with the responded value as false
+								email: email,
+								responded: false,
+							},
+						},
+					},
+					{
+						//after survey is found, update it
+						$inc: {
+							[choice]: 1, //update the choice value by 1
+						},
+						$set: {
+							'recipients.$.responded': true, //$ sets the original recipients id to the query
+						},
+					}
+				).exec();
+			})
+			.value();
+		res.send({});
 	});
 
 	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
