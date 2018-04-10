b3ac590720c72df8949575dab1ac9f6811c97e6e
James <james_gower@outlook.com>
2018-04-10 18:08:34 +0100

    better mobile font sizes and styling

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -9,15 +9,15 @@ const Dashboard = ({ surveys }) => {
 		<div>
 			<Header />
 			<div className="container" style={{marginBottom: '100px'}}>
-			<h4 style={{textAlign: 'center', marginBottom: '20px'}}>Dashboard</h4>
+			<h4 style={{textAlign: 'center', marginBottom: '20px', fontWeight: '700'}}>Dashboard</h4>
 			{ 
 				surveys.length === 0
-				? 	<div className="dashboard-text" style={{textAlign: 'center'}}>
+				? 	<div className="dashboard-text">
 						<p>There are no surveys to display. Please add send a survey using the + button for responses to display.</p>
 						<p>If you think this is an error, please contact us <a href="" target="_blank">here</a></p>
 						<p>To create a new survey, please click the red '+' button at the bottom of the page</p>
 					</div>
-				: 	<div className="dashboard-text"  style={{textAlign: 'center'}}>
+				: 	<div className="dashboard-text">
 						<p>Here are your current survey responses. Responses can take a minute or so for the click percentage to update, 
 						so please be patient while this loads.</p>
 						<p>To create a new survey, please click the red '+' button at the bottom right of the page</p>						
